424caf9ab874cfb902aa7dc7a31203a22389b9f6
Kotobukiyama <ibukisakagami@protonmail.com>
2026-07-19 13:55:26 -0300

    Working on the Cypher

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //We need text from the user
+    std::string pText{};
+    std::getline(std::cin >> std::ws, pText);
+
+    //We need a number of steps to jump in this cypher
+    int key{};
+    std::cin >> key;
+
+    for (size_t i = 0, n = pText.length(); i < n; i++)
+    {
+            if (isalpha(pText[i]))
+        {
+
+        }
+    }
+
+
     return 0;
 }
